018244b26dea1c5ea1cf88493b7a96fc3d2788ef
meyoramu <iyoramu@gmail.com>
2025-08-22 13:02:36 +0200

    docs(home): update categories label to food categories
    The label was changed to be more specific about what type of categories are being displayed, improving clarity for users.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -578,7 +578,7 @@ class _HomeState extends State<Home> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            "Categories",
+            "Food Categories",
             style: TextStyle(
               fontSize: 22,
               fontWeight: FontWeight.bold,
